388fd1447be45d3b6b80e012714453eeaf01c7fc
IngriKatherine <59847854+IngriKatherine@users.noreply.github.com>
2025-09-28 18:48:44 -0400

    EDA_newdata
    Add code to explore new dataset

---
+++ code/001_EDA_USfoodsurv.R
@@ -0,0 +1,10 @@
+# install if not already
+install.packages("haven")
+
+library(haven)
+
+setwd("C:/Users/k_the/Downloads")
+# path to your .xpt file
+demographics = read_xpt("DEMO_L.xpt")
+
+day1 = read_xpt("DR1IFF_L.xpt")
