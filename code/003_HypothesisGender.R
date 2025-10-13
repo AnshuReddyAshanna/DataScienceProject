66f0288961b09d05358beeff04ced82669b8c946
IngriKatherine <59847854+IngriKatherine@users.noreply.github.com>
2025-10-12 23:27:45 -0400

    Separated scripts

---
+++ code/003_HypothesisGender.R
@@ -0,0 +1,15 @@
+## This script does the Hypothesis testing: Do women on average, eat less protein than men?
+rm(list = ls())
+#### Preliminary
+# Libraries
+library(arrow)
+library(ggplot2)
+
+#Set Working Directory as the DataScienceProject folder
+getwd()  
+setwd("..")
+
+#### Reading the parquet file
+food <- read_parquet("proc_data/demo_foods.parquet")
+str(food)
+xkablesummary(food)
