1098f10f007f48780b42d4f9a49c15744095bf7b
dataondemandt <marlon.demandt@gwu.edu>
2025-11-30 21:13:52 -0500

    built a logistic regression model to predict whether someone follows a “healthy diet,” but instead of using all 7 strict criteria, I used a more realistic rule where people are counted as healthy if they meet at least 4 of them. Then I used the demographic variables (age, gender, education, income, ethnicity) to see what predicts that

--- code/Model3_LogisticRegression.R
+++ code/Model3_LogisticRegression.R
@@ -14,7 +14,7 @@ library(tidyr)
 library(vctrs)
 library(tibble)
 
-#Set Working Directory as the DataScienceProject folder
+#Set Working Directory 
 getwd()  
 setwd("/Users/marlon/Documents/EDUCATION/GWU/GWU Courses/GWU Fall 25/Introduction to Data Science_Darcy Morris/ModelDataWhatWeEat")
 
@@ -22,44 +22,65 @@ setwd("/Users/marlon/Documents/EDUCATION/GWU/GWU Courses/GWU Fall 25/Introductio
 food <- read_parquet("proc_data/demo_foods.parquet")
 head(food)
 
-# Healthy Diet Dummy Variable
-
-food <- food %>%
+# Create a health score (0–7) based on meeting dietary benchmark
+food = food %>%
   mutate(
-    healthy_diet =
-      if_else(
-        DR1IKCAL_sum <= 2000 &        # Calories
-          DR1ISODI_sum < 2300 &         # Sodium (mg)
-          DR1IPROT_sum >= 50 &          # Protein (g)
-          DR1ISUGR_sum <= 50 &          # Sugar (g)
-          DR1ICARB_sum <= 275 &         # Carbs (g)
-          DR1IFIBE_sum >= 28 &          # Fiber (g)
-          DR1ITFAT_sum <= 78,           # Fat (g)
-        1, 0
-      )
+    healthy_score =
+      (DR1IKCAL_sum <= 2000) +      # calories
+      (DR1ISODI_sum < 2300) +       # sodium
+      (DR1IPROT_sum >= 50) +        # protein
+      (DR1ISUGR_sum <= 50) +        # sugar
+      (DR1ICARB_sum <= 275) +       # carbohydrates
+      (DR1IFIBE_sum >= 28) +        # fiber
+      (DR1ITFAT_sum <= 78),         # fat
+    
+    # Define healthy diet = 1 if they meet at least 4 of 7 criteria (since all would be very rare)
+    healthy_diet = if_else(healthy_score >= 4, 1, 0)
   )
 
-### Demografic predictors
+# Convert to factor for log regression
+food$healthy_diet = factor(food$healthy_diet)
 
-model_data <- food %>%
+# Select demographic predictors and drop any missing values
+model_data = food %>%
   select(
     healthy_diet,
-    RIAGENDR,   # gender
-    RIDAGEYR,   # age
-    RIDRETH3,   # race (detailed)
-    DMDEDUC2,   # education
-    INDFMPIR    # income-to-poverty ratio
+    RIAGENDR,     # gender
+    RIDAGEYR,     # age
+    RIDRETH3,     # race/ethnicity
+    DMDEDUC2,     # education
+    INDFMPIR      # income-to-poverty ratio
   ) %>%
-  drop_na()   # logistic regression cannot run with NA
+  drop_na()
 
-
-## Fit regression 
-
-
-model3 <- glm(
+# Fit logistic regression predicting the odds of having a healthy diet based on 4 criterias
+model3 = glm(
   healthy_diet ~ RIAGENDR + RIDAGEYR + RIDRETH3 + DMDEDUC2 + INDFMPIR,
   data = model_data,
   family = binomial
 )
 
+#  regression results
 summary(model3)
+
+#The model shows that sex and age are the strongest predictors of meeting at least four healthy diet criteria. 
+#Women and older adults have higher odds of a healthier diet
+#Education has a small effect
+#income and race/ethnicity add little and are not strong predictors
+
+
+# Predicted probabilities of a healthy diet
+pred_prob3 = predict(model3, type = "response")
+
+# Convert probabilities to binary predictions using a 0.5 cutoff
+pred_class3 = if_else(pred_prob3 >= 0.5, 1, 0)
+
+# Confusion matrix comparing predictions to actual values
+conf_mat3 = table(pred_class3, model_data$healthy_diet)
+conf_mat3
+
+# Model accuracy
+accuracy3 = mean(pred_class3 == model_data$healthy_diet)
+accuracy3
+
+#using a 0.5 cutoff the model correctly predicts a healthy diet classification about 61.3% of the time
